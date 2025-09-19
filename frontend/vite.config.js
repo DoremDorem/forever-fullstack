67f8d941bf674bb4bc1b025471bff0083064c025
DoremDorem <doremdorem2@gmail.com>
2025-09-19 12:30:33 +0530

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  root: '.',            // ensure root is current folder
-  base: './',
-   build: {
-    outDir: 'dist',
-    emptyOutDir: true   // clear dist before build
-  },
   server: {
     port: 5173
-  },
+  }
 })
